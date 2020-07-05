65c7588b5749fef52daf96cecf20330458b62017
djy <137359049@qq.com>
2020-07-06 00:13:48 +0800

    1.信息流调试

--- app/src/main/java/com/beemans/topon/demo/constant/Constant.kt
+++ app/src/main/java/com/beemans/topon/demo/constant/Constant.kt
@@ -6,7 +6,12 @@ package com.beemans.topon.demo.constant
  */
 object Constant {
 
-    const val APP_ID = "a5efef38f5807f"
-    const val APP_KEY = "ce96a01d3a436ca15d2785a61b9701d7"
-    const val NATIVE_AD_ID = "b5efef3c883a00"
+//    const val APP_ID = "a5efef38f5807f"
+//    const val APP_KEY = "ce96a01d3a436ca15d2785a61b9701d7"
+//    const val NATIVE_AD_ID = "b5efef3c883a00"
+
+    const val APP_ID = "a5aa1f9deda26d"
+    const val APP_KEY = "4f7b9ac17decb9babec83aac078742c7"
+    const val NATIVE_AD_ID = "b5c2c97629da0d"
+    const val NATIVE_AD_ID2 = "b5d148f9f2e47d"
 }
