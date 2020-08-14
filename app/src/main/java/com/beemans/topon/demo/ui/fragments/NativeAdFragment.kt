40b1775617980a33f8d0fef90e927538491569a5
djy <137359049@qq.com>
2020-08-14 10:24:24 +0800

    1.信息流优化

--- app/src/main/java/com/beemans/topon/demo/ui/fragments/NativeAdFragment.kt
+++ app/src/main/java/com/beemans/topon/demo/ui/fragments/NativeAdFragment.kt
@@ -7,7 +7,6 @@ import com.beemans.topon.demo.base.BaseFragment
 import com.beemans.topon.demo.constant.Constant
 import com.beemans.topon.demo.ext.pt2px
 import com.beemans.topon.nativead.NativeAdConfig
-import com.beemans.topon.nativead.NativeAdLoader
 import kotlinx.android.synthetic.main.fragment_native_ad.*
 
 /**
@@ -15,41 +14,29 @@ import kotlinx.android.synthetic.main.fragment_native_ad.*
  * @date 2020/7/5.
  */
 class NativeAdFragment : BaseFragment() {
-    private var nativeAdLoader: NativeAdLoader? = null
     private val config by lazy { NativeAdConfig(Constant.NATIVE_AD_ID, 350.pt2px, 270.pt2px) }
 
     override fun getLayoutId() = R.layout.fragment_native_ad
 
     override fun initEvent() {
-        //预加载广告1
-        nativeAd_btnPreload1.setOnClickListener {
+        //预加载广告
+        nativeAd_btnPreload.setOnClickListener {
             TopOn.loadNativeAd(this, config)
-        }
-
-        //预加载广告2
-        nativeAd_btnPreload2.setOnClickListener {
-            if (nativeAdLoader == null) {
-                nativeAdLoader = TopOn.loadNativeAd(this, config) {
-                    onAdRenderSuc { flAdView ->
-                        nativeAd_flAd.addView(flAdView)
-                    }
-                    onAdLoadFail {
-                        Log.e("xia", "${this.javaClass.simpleName}show:${it?.printStackTrace()}")
-                    }
-                    onAdCloseClick { _, _ ->
-                        true
-                    }
-                }
-            }
         }
 
         //显示广告
         nativeAd_btnShowAd.setOnClickListener {
-            if (nativeAdLoader != null) {
-                nativeAdLoader?.show()
-                return@setOnClickListener
-            }
-            TopOn.loadNativeAd(this, config).show()
+            TopOn.loadNativeAd(this, config) {
+                onAdRenderSuc { flAdView ->
+                    nativeAd_flAd.addView(flAdView)
+                }
+                onAdLoadFail {
+                    Log.e("xia", "${this.javaClass.simpleName}show:${it?.printStackTrace()}")
+                }
+                onAdCloseClick { _, _ ->
+                    true
+                }
+            }.show()
         }
     }
 }
