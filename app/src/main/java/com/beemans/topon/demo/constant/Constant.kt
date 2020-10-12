be276a1b8c71fda95fa8d68ebe91ad7044ed08ef
djy <137359049@qq.com>
2020-10-12 18:13:10 +0800

    1.集成快手广告

--- app/src/main/java/com/beemans/topon/demo/constant/Constant.kt
+++ app/src/main/java/com/beemans/topon/demo/constant/Constant.kt
@@ -5,19 +5,19 @@ package com.beemans.topon.demo.constant
  * @date 2020/7/5.
  */
 object Constant {
-//    const val APP_ID = "a5efef38f5807f"
-//    const val APP_KEY = "ce96a01d3a436ca15d2785a61b9701d7"
-//    const val NATIVE_AD_ID = "b5efef3c883a00"
-//    const val REWARD_ID = "b5f1502786c59e"
-//    const val INTERSTITIAL_ID = "b5f1504051d3b2"
-//    const val BANNER_ID = "b5baca45138428"
-//    const val SPLASH_ID = "b5f07004e8a85f"
-
-    const val APP_ID = "a5aa1f9deda26d"
-    const val APP_KEY = "4f7b9ac17decb9babec83aac078742c7"
-    const val NATIVE_AD_ID = "b5c2c97629da0d"
+    const val APP_ID = "a5efef38f5807f"
+    const val APP_KEY = "ce96a01d3a436ca15d2785a61b9701d7"
+    const val NATIVE_AD_ID = "b5efef3c883a00"
     const val REWARD_ID = "b5f486a5a9a1ae"
-    const val INTERSTITIAL_ID = "b5baca585a8fef"
+    const val INTERSTITIAL_ID = "b5f1504051d3b2"
     const val BANNER_ID = "b5baca45138428"
-    const val SPLASH_ID = "b5bea7c1b653ef"
+    const val SPLASH_ID = "b5f07004e8a85f"
+
+//    const val APP_ID = "a5aa1f9deda26d"
+//    const val APP_KEY = "4f7b9ac17decb9babec83aac078742c7"
+//    const val NATIVE_AD_ID = "b5c2c97629da0d"
+//    const val REWARD_ID = "b5f486a5a9a1ae"
+//    const val INTERSTITIAL_ID = "b5baca585a8fef"
+//    const val BANNER_ID = "b5baca45138428"
+//    const val SPLASH_ID = "b5bea7c1b653ef"
 }
--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -2,17 +2,17 @@
 
 object Android {
     const val compileSdkVersion = 30
-    const val buildToolsVersion = "30.0.1"
+    const val buildToolsVersion = "30.0.2"
     const val minSdkVersion = 21
     const val targetSdkVersion = 30
 
-    const val versionName = "1.0.0"
+    const val versionName = "1.5.8"
     const val versionCode = 1
 }
 
 object Versions {
-    const val kotlin = "1.4.0"
-    const val fly = "1.4.8"
+    const val kotlin = "1.4.10"
+    const val fly = "1.5.4"
     const val leakcanary = "2.4"
 }
 
@@ -23,7 +23,7 @@ object Deps {
     const val fly_imageloader_glide = "me.tiamosu:fly-imageloader-glide:${Versions.fly}"
 
     // androidx
-    const val androidx_constraint_layout = "androidx.constraintlayout:constraintlayout:2.0.0"
+    const val androidx_constraint_layout = "androidx.constraintlayout:constraintlayout:2.0.2"
     const val androidx_viewpager2 = "androidx.viewpager2:viewpager2:1.0.0"
 
     // leakcanary 检测内存泄漏
